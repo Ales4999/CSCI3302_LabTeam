5e92cce81e4e21e8076bf4ac4bd54a59567d9ab2
Ales4999 <ales4999@colorado.edu>
2023-02-15 16:56:35 -0700

    Mark Soln

--- lab2/controllers/lab2_controller/lab2_controller.py
+++ lab2/controllers/lab2_controller/lab2_controller.py
@@ -9,7 +9,8 @@ import os
 # Ground Sensor Measurements under this threshold are black
 # measurements above this threshold can be considered white.
 # TODO: Fill this in with a reasonable threshold that separates "line detected" from "no line detected"
-GROUND_SENSOR_THRESHOLD = 0
+# when the gs detects the line, the signal drops to arounnd 300, so 350 is an appropriate threshold
+GROUND_SENSOR_THRESHOLD = 350
 
 # These are your pose values that you will update by solving the odometry equations
 pose_x = 0
@@ -25,8 +26,9 @@ RIGHT_IDX = 0
 robot = Robot()
 
 # ePuck Constants
-EPUCK_AXLE_DIAMETER = 0.053 # ePuck's wheels are 53mm apart.
-EPUCK_MAX_WHEEL_SPEED = 0.1163 # TODO: To be filled in with ePuck wheel speed in m/s
+EPUCK_AXLE_DIAMETER = 0.053  # ePuck's wheels are 53mm apart.
+# TODO: To be filled in with ePuck wheel speed in m/s
+EPUCK_MAX_WHEEL_SPEED = 0.1163
 MAX_SPEED = 6.28
 
 # get the time step of the current world.
@@ -42,15 +44,17 @@ rightMotor.setVelocity(0.0)
 
 # Initialize and Enable the Ground Sensors
 gsr = [0, 0, 0]
-ground_sensors = [robot.getDevice('gs0'), robot.getDevice('gs1'), robot.getDevice('gs2')]
+ground_sensors = [robot.getDevice('gs0'), robot.getDevice(
+    'gs1'), robot.getDevice('gs2')]
 for gs in ground_sensors:
     gs.enable(SIM_TIMESTEP)
 
 # Allow sensors to properly initialize
-for i in range(10): robot.step(SIM_TIMESTEP)  
+for i in range(10):
+    robot.step(SIM_TIMESTEP)
 
-vL = 0 # TODO: Initialize variable for left speed
-vR = 0 # TODO: Initialize variable for right speed
+vL = 0.5*MAX_SPEED  # TODO: Initialize variable for left speed
+vR = 0.5*MAX_SPEED  # TODO: Initialize variable for right speed
 
 # Main Control Loop:
 while robot.step(SIM_TIMESTEP) != -1:
@@ -59,9 +63,9 @@ while robot.step(SIM_TIMESTEP) != -1:
     for i, gs in enumerate(ground_sensors):
         gsr[i] = gs.getValue()
 
-    #print(gsr) # TODO: Uncomment to see the ground sensor values!
+    print(gsr)  # TODO: Uncomment to see the ground sensor values!
 
-    # Hints: 
+    # Hints:
     #
     # 1) Setting vL=MAX_SPEED and vR=-MAX_SPEED lets the robot turn
     # right on the spot. vL=MAX_SPEED and vR=0.5*MAX_SPEED lets the
@@ -75,21 +79,45 @@ while robot.step(SIM_TIMESTEP) != -1:
     # 4) Disable all console output to simulate the robot superfast
     # and test the robustness of your approach.
     #
-    
-    # TODO: Insert Line Following Code Here                
-    
-    
-    
-    
-    
-    
-    
+
+    # TODO: Insert Line Following Code Here
+
+    # Store the values of the sensors
+    gs_left = gsr[0]
+    gs_center = gsr[1]
+    gs_right = gsr[2]
+
+    # If epuck is on the line
+    if (gs_center < gs_right) and (gs_center < gs_left) and (gs_center < GROUND_SENSOR_THRESHOLD):
+        vL = 0.5*MAX_SPEED
+        vR = 0.5*MAX_SPEED
+
+    # If epuck is on the startline
+    elif (gs_right < GROUND_SENSOR_THRESHOLD) and (gs_left < GROUND_SENSOR_THRESHOLD) and (gs_center < GROUND_SENSOR_THRESHOLD):
+        vL = 0.5*MAX_SPEED
+        vR = 0.5*MAX_SPEED
+
+    # If epuck senses line curving to the left
+    elif (gs_left < gs_right) and (gs_left < gs_center) and (gs_left < GROUND_SENSOR_THRESHOLD):
+        vR = 0.5*MAX_SPEED
+        vL = -0.5*MAX_SPEED
+
+    # If epuck senses line curving to the right
+    elif (gs_left > gs_right) and (gs_right < gs_center) and (gs_right < GROUND_SENSOR_THRESHOLD):
+        vR = -0.5*MAX_SPEED
+        vL = 0.5*MAX_SPEED
+
+    # If none of the ground sensors detect the line
+    elif (gs_right > GROUND_SENSOR_THRESHOLD) and (gs_left > GROUND_SENSOR_THRESHOLD) and (gs_center > GROUND_SENSOR_THRESHOLD):
+        vR = 0.5*MAX_SPEED
+        vL = -0.5*MAX_SPEED
+
     # TODO: Call update_odometry Here
-    
+
     # Hints:
     #
     # 1) Divide vL/vR by MAX_SPEED to normalize, then multiply with
-    # the robot's maximum speed in meters per second. 
+    # the robot's maximum speed in meters per second.
     #
     # 2) SIM_TIMESTEP tells you the elapsed time per step. You need
     # to divide by 1000.0 to convert it to seconds
@@ -101,18 +129,15 @@ while robot.step(SIM_TIMESTEP) != -1:
     # about calculating odometry in the world coordinate system of the
     # Webots simulator first (x points down, y points right)
 
-    
-
-    
     # TODO: Insert Loop Closure Code Here
-    
+
     # Hints:
     #
     # 1) Set a flag whenever you encounter the line
     #
-    # 2) Use the pose when you encounter the line last 
+    # 2) Use the pose when you encounter the line last
     # for best results
-    
+
     print("Current pose: [%5f, %5f, %5f]" % (pose_x, pose_y, pose_theta))
     leftMotor.setVelocity(vL)
     rightMotor.setVelocity(vR)
