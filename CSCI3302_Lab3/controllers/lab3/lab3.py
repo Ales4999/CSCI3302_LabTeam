dc1d328667c098808cb59b662ac76dd18791e0bf
Mark Abbott (S) <maab2019@Marks-MacBook-Pro-3.local>
2023-02-23 23:39:57 -0700

    Made some headway on Lab3

--- CSCI3302_Lab3/controllers/lab3/lab3.py
+++ CSCI3302_Lab3/controllers/lab3/lab3.py
@@ -10,15 +10,16 @@ MAX_SPEED = 6.67 # [rad/s]
 MAX_SPEED_MS = 0.22 # [m/s]
 AXLE_LENGTH = 0.178 # [m]
 
-
 MOTOR_LEFT = 0 # Left wheel index
 MOTOR_RIGHT = 1 # Right wheel index
+
 
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+timestep_s = timestep / 10000
 
 # The Turtlebot robot has two motors
 part_names = ("left wheel motor", "right wheel motor")
@@ -47,58 +48,85 @@ pose_theta = 0
 vL = 0
 vR = 0
 
-#Waypoints, array of (x,y) coordinates to traverse 
-#Fill with correct values!!
-waypoints  = [[0, 1],
-              [2, 3],
-              [4, 5],
-              [6, 7],
-              [8, 9],
-              [10,11]]
-
 # TODO
 # Create you state and goals (waypoints) variable here
 # You have to MANUALLY figure out the waypoints, one sample is provided for you in the instructions
+# Waypoints, array of (x,y) coordinates to traverse 
+# Fill with correct values!!
+
+# waypoints  = [[0, 1],
+#               [2, 3],
+#               [4, 5],
+#               [6, 7],
+#               [8, 9],
+#               [10,11]]
 
 while robot.step(timestep) != -1:
 
     # STEP 2.1: Calculate error with respect to current and goal position
     
-    #Prof said in class odometry was going to be given  
-    #down the line this must be updated somehow
-    goal_x = waypoints[0][0]
-    goal_y = waypoints[0][1] 
+    #goal_x = waypoints[0][0]
+    #goal_y = waypoints[0][1] 
+    goal_x = 5
+    goal_y = 6
+    goal_theta = 90
     
+    ## Position Error : rho ##
     # sqrt( (xr-xg)^2 + (yr - yg)^2) 
-    rho = math.sqrt( (pose_x - goal_x)^2  + (pose_y - goal_y )^2)
-    pass   
+    rho = math.sqrt( (pose_x - goal_x)**2  + (pose_y - goal_y )**2)
+    print("-> rho:", rho)
     
     # STEP 2.2: Feedback Controller
-    pass
+    ## Bearing Error: alpha##
+    alpha = math.atan2((goal_y-pose_y),(goal_x-pose_x))-pose_theta
+    print("-> alpha:", alpha)
+ 
+    # STEP 1: Inverse Kinematics Equations (vL and vR as a function dX and dTheta)
+    # Note that vL and vR in code is phi_l and phi_r on the slides/lecture
     xR_dot = 0
     thetaR_dot = 0
     radius =  (MAX_SPEED_MS/MAX_SPEED)
     d = AXLE_LENGTH
-    # STEP 1: Inverse Kinematics Equations (vL and vR as a function dX and dTheta)
-    # Note that vL and vR in code is phi_l and phi_r on the slides/lecture
     vL = xR_dot - (thetaR_dot/2)*d
     vR = xR_dot + (thetaR_dot/2)*d
 
-    xR_dot =(vL/2 + vR/2)
-    thetaR_dot = (vR/d - vL/d)
+    xR_dot = vL/2 + vR/2
+    thetaR_dot = vR/d - vL/d
 
-    pass
     
     # STEP 2.3: Proportional velocities
     vL = 0 # Left wheel velocity in rad/s
     vR = 0 # Right wheel velocity in rad/s
-    pass
+    ## Heading Error: nu ##
+    nu = goal_theta - pose_theta
 
     # STEP 2.4: Clamp wheel speeds
-    pass
+    #controller gains p1, p2, p3
+    #xR_dot = p1 * rho 
+    #thetaR_dot = p2 * alpha + p3 * nu
 
+    #need conditional logic to determine controller gains
+    #Prioritize BEaring error
+    p1, p2, p3 = 0
+    if( abs(alpha) > 0.1 ):
+        p1 = 0.1 #small gains for rho
+        p2 = 0.8 #higher gains for alpha 
+        p3= 0  #no update for nu 
+    else:
+        #Prioratize position error
+        if( abs(rho) > 0.1 ):
+            p1 = 0.8
+            p2 = 0.1 
+            p3= 0
+        else:
+            #Prioritize heading error
+            p1 = 0.1
+            p2 = 0.1
+            p3= 0.8
 
-    
+    xR_dot = p1 * rho 
+    ThetaR_dot = p2 * alpha + p3 * nu
+             
     # TODO
     # Use Your Lab 2 Odometry code after these 2 comments. We will supply you with our code next week 
     # after the Lab 2 deadline but you free to use your own code if you are sure about its correctness
@@ -107,7 +135,21 @@ while robot.step(timestep) != -1:
     # (vL, vR, MAX_SPEED, MAX_SPEED_MS, timestep, AXLE_LENGTH, pose_x, pose_y, pose_theta)
     # Odometry code. Don't change speeds (vL and vR) after this line
     
-    
+    Rx_dot = vR/2 + vL/2 #Forward speed of the robot
+    Rw_dot = vR/d - vL/d #Net rotation in the local frame
+
+    ix_dot = Rx_dot * math.cos(pose_theta) #Robots x_dot contribution in i coordinate frame
+    iy_dot = Rx_dot * math.sin(pose_theta) #Robots y_dot contribution in i coordinate frame
+    itheta_dot = Rw_dot #Robots theta_dot contribution in i coordinate frame
+
+    if (vR*vL < 0):
+        pose_theta += itheta_dot*timestep_s #multiply by change in t to find rotation and add to previous value
+    #If the robot is moving straight, update its x and y values
+    else:
+        pose_x += ix_dot*timestep_s #multiply by change in t to find displacement and add to previous value
+        pose_y += iy_dot*timestep_s #multiply by change in t to find displacement and add to previous value
+
+
     
 
     ########## End Odometry Code ##################
